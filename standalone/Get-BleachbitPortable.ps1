ece4c407c138e69b2740f91ceacb1ccf2a5c74b7
Markus Kofler <markus.kofler@kofler-it.com>
2021-01-05 17:22:06 +0100

    Update Get-BleachbitPortable to version 4.2.0

--- standalone/Get-BleachbitPortable.ps1
+++ standalone/Get-BleachbitPortable.ps1
@@ -2,11 +2,11 @@
 # Author:   Markus Kofler (mkoflerAT)
 # Website:  https://www.kofler-it.com
 # Licence:  MIT
-# Version:  1.0.0.0
-# Date:     2020-11-11 03:00
+# Version:  1.1.0.0
+# Date:     2021-01-05 17:20
 # ====================================================================================================
 function Get-BleachbitPortable {
-    $uri = 'https://download.bleachbit.org/BleachBit-4.0.0-portable.zip'
+    $uri = 'https://download.bleachbit.org/BleachBit-4.2.0-portable.zip'
     $out = [Environment]::GetFolderPath("Desktop") + "\BleachBitPortable.zip"
     (New-Object System.Net.WebClient).DownloadFile($uri, $out)
     $targetPath = [Environment]::GetFolderPath("Desktop")
